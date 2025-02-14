f4d25da23c917a5322a03871bd14d6651e6b9295
vinicius_leo <joaoviniciusleo72@gmail.com>
2025-02-13 21:09:51 -0300

    Add files via upload

---
+++ atividadefront.py
@@ -0,0 +1,9 @@
+#pegar as alturas
+#separar os homens e as mulheres
+#calcular a média de altura de cada grupo
+#mostrar a média de altura de cada grupo
+for i in range (15):
+    input('digite seu genero em F ou M:')
+    float(input('digite sua altura em CM:'))
+#comparar as medias de altura dos homens e das mulheres
+#mostrar a altura maxima dos homens e das mulheres
